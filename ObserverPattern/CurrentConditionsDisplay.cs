3dbdcf48a7f42d7a1b2a544d34bfd10cb495df0c
Md. Waliullah <waliullahmarjan@gmail.com>
2024-10-06 09:29:11 +0600

    Observer pattern update
    - Added new display element
    - Added tests

---
+++ ObserverPattern/CurrentConditionsDisplay.cs
@@ -0,0 +1,29 @@
+﻿using ObserverPattern.Interfaces;
+
+namespace ObserverPattern
+{
+    internal class CurrentConditionsDisplay : IObserver, IDisplayElement
+    {
+        private float Temperature { get; set; }
+        private float Humidity { get; set; }
+        private ISubject WeatherData { get; set; }
+
+        public CurrentConditionsDisplay(ISubject weatherData)
+        {
+            WeatherData = weatherData;
+            WeatherData.RegisterObserver(this);
+        }
+
+        public void Update(float temp, float humidity, float pressure)
+        {
+            Temperature = temp;
+            Humidity = humidity;
+            Display();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Current Conditions: {Temperature} degree C and {Humidity}% humidity.");
+        }
+    }
+}
--- ObserverPattern/Program.cs
+++ ObserverPattern/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Observer Pattern!");
+
+            WeatherData weatherData = new WeatherData();
+
+            CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+
+            weatherData.SetMeasurements(28, 65, 30.4f);
+            weatherData.SetMeasurements(30, 70, 29.2f);
+            weatherData.SetMeasurements(26, 90, 29.2f);
         }
     }
 }
